9cb654ac98ea5d2f1e4c499f240748248fd19471
David Nordeen <davidnordeen@gmail.com>
2019-08-19 19:36:55 -0700

    edit to config

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL
+  DB_URL: process.env.DATABASE_URL || 'postgres://hcjfatvwhuoijh:fed5fe971f40e96da9c36ae3aaf8afa0fa74a5746b3d222d69a887c67bf2f31e@ec2-174-129-226-234.compute-1.amazonaws.com:5432/de763uvi6fqch8'
 };
