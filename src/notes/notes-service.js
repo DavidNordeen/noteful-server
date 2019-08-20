cb5344372b2175c0b162dc6023a2f924f9b0cbb2
David Nordeen <davidnordeen@gmail.com>
2019-08-19 18:41:40 -0700

    change notes service

--- src/notes/notes-service.js
+++ src/notes/notes-service.js
@@ -1,26 +1,27 @@
+'use strict';
 const NotesService = {
-    getAllNotes(knex) {
-        found = knex.select('*').from('notes');
-        return found
-    },
-    insertNote(knex, insertNote) {
-        return knex.insert(insertNote).into('notes');
-    },
-    patchNote(knex, patchNote) {
-        return knex('notes').where('id', patchNote.id).update(patchNote);
-    },
-    deleteNote(knex, id){
-        return knex('notes')
-        .where('id', id)
-        .del()
-    },
-    updateNote(knex, insertNote){
+  getAllNotes(knex) {
+    const found = knex.select('*').from('notes');
+    return found;
+  },
+  insertNote(knex, insertNote) {
+    return knex.insert(insertNote).into('notes');
+  },
+  patchNote(knex, patchNote) {
+    return knex('notes').where('id', patchNote.id).update(patchNote);
+  },
+  deleteNote(knex, id) {
+    return knex('notes')
+      .where('id', id)
+      .del();
+  },
+  updateNote(knex, insertNote) {
 
-    },
-    getNoteById(knex, id) {
-       const found = knex.from('notes').select('*').where('id', id).first()
-       return found
-    }
+  },
+  getNoteById(knex, id) {
+    const found = knex.from('notes').select('*').where('id', id).first();
+    return found;
   }
+};
 
 module.exports = NotesService;
